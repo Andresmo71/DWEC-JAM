c8d34c7919510b4efa696593553445c81ed3933c
Andresmo71 <jose.maranon@educa.madrid.org>
2025-09-25 13:52:27 +0200

    Add files via upload

---
+++ UD2-Manejo de la sintaxis de JS/ejercicio8.js
@@ -0,0 +1,17 @@
+let bisiesto =(ano) => {
+    let anoCopia=parseInt(ano)
+    let cadena=toString(ano)
+    let copia=""
+    let boolean=false
+    while (boolean===false){
+        if (cadena.lastIndexOf==="0"){
+            anoCopia=anoCopia/10
+        }
+        else{
+            boolean=true
+        }
+    }
+
+
+    
+}
