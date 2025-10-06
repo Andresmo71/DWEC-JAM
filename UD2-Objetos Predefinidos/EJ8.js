85d0aec624f882baa23994620c35ad248a3396ae
Andresmo71 <jose.maranon@educa.madrid.org>
2025-10-06 11:51:04 +0200

    Add files via upload

---
+++ UD2-Objetos Predefinidos/EJ8.js
@@ -0,0 +1,14 @@
+const estudiantes = [
+  "Ana Garcia",
+  "Luis Martinez",
+  "Maria Lopez",
+  "Carlos Rodriguez",
+  "Elena Sanchez",
+  "Pedro Hernandez"
+];
+const grupoExtraido= estudiantes.slice(2,2)
+let listaEstudiantes=estudiantes.join("|")
+const primerEtudiante=estudiantes[0].split(" ")
+console.log(estudiantes)
+console.log(grupoExtraido)
+console.log(primerEtudiante)
